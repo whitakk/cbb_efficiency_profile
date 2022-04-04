779d0844bfcbb2d46601d31e5db8a2a1612228c0
whitakk <whitakk@gmail.com>
2022-04-04 13:56:39 -0400

    update download script to add new tables

--- get_trank_data.py
+++ get_trank_data.py
@@ -65,6 +65,63 @@ def save_season_stats():
     master_team_stats.to_csv('../data/team_stats.csv', index=False)
 
 
+def save_pbp_stats():
+    """get shooting splits for each team in all available years"""
+
+    end_year = get_current_year()
+    years = range(2010, end_year+1)
+    # first year of pbp data is 2010
+    # note this always includes the current season
+
+    master_team_stats = pd.DataFrame()
+    for year in years:
+        year_df = pd.read_json(f'https://barttorvik.com/{year}_pbp_teamsstats.json')
+        print(len(year_df), "team shooting splits found for", year)
+
+        # syntax of response data: 'offense' : [close2_made, close2_missed, ??, long2_made long2_missed, ??, 3_made, 3_missed, ??]
+
+        year_df['close2_pct'] = year_df['offense'].apply(lambda x: x[0]/(x[0]+x[1]))
+        year_df['close2_share'] = year_df['offense'].apply(lambda x: (x[0]+x[1])/(x[0]+x[1]+x[3]+x[4]+x[6]+x[7]))
+        
+        year_df['long2_pct'] = year_df['offense'].apply(lambda x: x[3]/(x[3]+x[4]))
+        year_df['long2_share'] = year_df['offense'].apply(lambda x: (x[3]+x[4])/(x[0]+x[1]+x[3]+x[4]+x[6]+x[7]))
+
+        # threes are already in the team_stats data
+
+        year_df['season'] = year
+        year_df['team'] = year_df.index
+        master_team_stats = master_team_stats.append(year_df).reset_index(drop=True)
+
+    out_cols = ['season','team','close2_pct','close2_share','long2_pct','long2_share']
+    master_team_stats[out_cols].to_csv('../data/pbp_stats.csv', index=False)
+    return 
+
+
+def save_season_efficiency():
+    """get overall efficiency for each team in all available years"""
+
+    end_year = get_current_year()
+    years = range(2008, end_year+1)
+    # note this always includes the current season
+
+    efficiencies = pd.DataFrame()
+    for year in years:
+        year_df = pd.read_csv(f'http://barttorvik.com/{year}_team_results.csv')
+        print(len(year_df), "team efficiencies found for", year)
+
+        # raw columns are mismatched (first four are an index column, not labeled as such)
+        year_df = year_df.reset_index()
+        year_df.columns = list(
+            year_df.columns[1:])+['tmp']
+
+        year_df['season'] = year
+        efficiencies = efficiencies.append(year_df).reset_index(drop=True)
+
+    out_cols = ['season','team','adjoe','adjde']
+    efficiencies[out_cols].to_csv('../data/team_efficiency.csv', index=False)
+    return 
+    
+
 def get_current_year():
     """get the year corresponding to the current (or most recent) season"""
     
@@ -73,8 +130,10 @@ def get_current_year():
 
 
 def main():
-    save_game_stats()
-    save_season_stats()
+    # save_game_stats()
+    # save_season_stats()
+    # save_pbp_stats()
+    save_season_efficiency()
 
 
 main()
